44635cfa9fe1c944af2123497d8b57520c06de27
chaeriz <chrisobt@outlook.com>
2024-06-11 11:43:15 +0200

    init search

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,18 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
+use Symfony\Component\HttpFoundation\Response;
 class Handler extends ExceptionHandler
 {
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            return response()->json(['error' => 'Unauthenticated.'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return parent::render($request, $exception);
+    }
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
