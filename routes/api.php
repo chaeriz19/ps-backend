47ec8f76007defc1111137f616d142694113c126
chaeriz <chrisobt@outlook.com>
2024-06-11 15:36:07 +0200

    Authenticatio fixed (error when not passing token)

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,24 @@
-<?php
+    <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\AuthController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\MovieController;
+    use App\Http\Controllers\AuthController;
 
-// authentication routes
-Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
-Route::post('/user/register', [AuthController::class, 'register'])->name('user.register');
+    // authentication routes
+    Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
+    Route::post('/user/register', [AuthController::class, 'register'])->name('user.register');
 
 
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/movies/all', [MovieController::class, 'all'])->name('movies.all');
-    Route::get('/movies/latest', [MovieController::class, 'latest'])->name('movies.latest');
-    Route::post('/movies/page', [MovieController::class, 'page_control'])->name('movies.page_control');
-    Route::post('/movies/search', [MovieController::class, 'search'])->name('movies.search');
-});
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/movies/all', [MovieController::class, 'all'])->name('movies.all');
+        Route::get('/movies/latest', [MovieController::class, 'latest'])->name('movies.latest');
+        Route::post('/movies/page', [MovieController::class, 'page_control'])->name('movies.page_control');
+        Route::post('/movies/search', [MovieController::class, 'search'])->name('movies.search');
+    });
+
+        
+
 
 
 
