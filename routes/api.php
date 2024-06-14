00706a9e8cd9019dc6ec69a823212b92b9d65413
chaeriz <chrisobt@outlook.com>
2024-06-14 13:24:15 +0200

    Add all movie route to public

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AuthController;
 // authentication routes
 Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
 Route::post('/user/register', [AuthController::class, 'register'])->name('user.register');
+Route::get('/movies/all', [MovieController::class, 'all'])->name('movies.all');
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -15,7 +16,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/remove-favorite', [MovieController::class, 'remove_favorite'])->name('users.remove_favorite');
     Route::post('/user/get-favorite', [MovieController::class, 'get_favorites'])->name('users.get_favorites');
 
-    Route::get('/movies/all', [MovieController::class, 'all'])->name('movies.all');
     Route::get('/movies/latest', [MovieController::class, 'latest'])->name('movies.latest');
     Route::post('/movies/page', [MovieController::class, 'page_control'])->name('movies.page_control');
     Route::post('/movies/search', [MovieController::class, 'search'])->name('movies.search');
