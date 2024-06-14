2b073e8633a4b962de9f805c5b624275de67debc
chaeriz <chrisobt@outlook.com>
2024-06-14 13:43:53 +0200

    Fix CORS

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -1,35 +1,18 @@
-<?php namespace App\Http\Middleware;
+
+namespace App\Http\Middleware;
 
 use Closure;
-use Symfony\Component\HttpFoundation\Response;
-class CORS {
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+class CORS
+{
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        header("Access-Control-Allow-Origin: *");
 
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if ($request->getMethod() == 'OPTIONS') {
-            $response->setStatusCode(200);
-            $response->setContent('OK');
-        }
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
         return $response;
     }
-
 }
