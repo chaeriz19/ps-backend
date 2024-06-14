556c5d826bf52ff7120c73ff6885082ab74dc6fe
Wesley <124563500+Wesleyx1337@users.noreply.github.com>
2024-06-14 14:56:18 +0200

    Page control serie

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SerieController extends Controller
 {
-    //
+    public function page_control(Request $request) {
+
+        $request->validate([
+            'page' => 'required',
+        ]);
+
+        $page = $request->page;
+        $perPage = 10;
+
+        $data = Movie::orderBy('created_at', 'desc')
+                    ->skip(($page - 1) * $perPage)
+                    ->take($perPage)
+                    ->get();
+
+        return response()->json($data);
+    }
 }
