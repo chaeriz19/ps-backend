a1a3bb62585cecc8198dfcf0e3e595c834d71113
chaeriz <chrisobt@outlook.com>
2024-07-04 16:01:10 +0200

    Updates

--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,29 @@ Route::get('/all/{type}', [ContentController::class, 'get_all'])->name('get.all'
 Route::get('/latest', [ContentController::class, 'latest'])->name('get.latest');
 Route::get('/genre/{genre}', [ContentController::class, 'sort_genre'])->name('get.latest');
 
+
+
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     
+
+
+    // protected content routes
     Route::post('/content/create', [ContentController::class, 'create_content'])->name('content.create');
     Route::post('/content/delete', [ContentController::class, 'delete_content'])->name('content.delete');
     Route::post('/content/all', [ContentController::class, 'all'])->name('content.all');
 
-
+    // protected user routes
     Route::post('/user/delete', [AuthController::class, 'delete'])->name('user.delete');
     Route::post('/user/logout', [AuthController::class, 'logout'])->name('user.logout');
     Route::post('/user/toggle', [FavoriteController::class, 'togglefavorite'])->name('favorite.togglefavorite');
     Route::post('/user/favorites', [FavoriteController::class, 'showfavorites'])->name('favorite.showfavorites');
     Route::post('/user/check', [FavoriteController::class, 'checkfavorite'])->name('user.checkfavorite');
-    Route::post('/search', [ContentController::class, 'search'])->name('search');
-
     Route::get('/user/get', [AuthController::class, 'getUser'])->name('user.get');
     Route::post('/user/delete', [AuthController::class, 'deleteUser'])->name('user.delete');
     Route::post('/user/edit', [AuthController::class, 'editUser'])->name('user.edit');
+
+    Route::post('/search', [ContentController::class, 'search'])->name('search');
 });
 
 // Niet protected
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/csrf', function() {
+        return response()->json(['csrfToken' => csrf_token()]);
+    });
+});
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
